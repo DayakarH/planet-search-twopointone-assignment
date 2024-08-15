acb018548356d88df5061c066d84e6990ee3e01d
Dayakar H <dayakarheeraakri@gmail.com>
2024-08-15 11:12:11 +0530

    add services to root store config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchAndFiltersSlice from "./search-and-filters-slice";
+import { planetsAPI } from "@/services/planets";
 
 const store = configureStore({
   reducer: {
     searchAndFiltersSlice,
+    [planetsAPI.reducerPath]: planetsAPI.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(planetsAPI.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
