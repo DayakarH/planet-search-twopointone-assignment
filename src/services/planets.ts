d18cbc09eebd7b9933b2a6b832737f6db46309c1
Dayakar H <dayakarheeraakri@gmail.com>
2024-08-15 11:09:44 +0530

    create services with RTK Query to fetch planets and filter options

---
+++ src/services/planets.ts
@@ -0,0 +1,35 @@
+import { Filters, type FilterOption, type Planet } from "@/lib/types";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const planetsAPI = createApi({
+  reducerPath: "planetsAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
+  endpoints: (builder) => ({
+    searchPlanets: builder.query<Array<Planet>, Filters>({
+      query: ({ searchText, filters }) => {
+        const params = new URLSearchParams();
+        if (searchText) params.append("q", searchText);
+        Object.entries(filters).forEach(([key, values]) => {
+          values.forEach((value) => params.append(key, value));
+        });
+        return `planets?${params.toString()}`;
+      },
+    }),
+    getColorOptions: builder.query<Array<FilterOption>, void>({
+      query: () => "colors",
+    }),
+    getShapeOptions: builder.query<Array<FilterOption>, void>({
+      query: () => "shapes",
+    }),
+    getSizeOptions: builder.query<Array<FilterOption>, void>({
+      query: () => "sizes",
+    }),
+  }),
+});
+
+export const {
+  useSearchPlanetsQuery,
+  useGetColorOptionsQuery,
+  useGetShapeOptionsQuery,
+  useGetSizeOptionsQuery,
+} = planetsAPI;
