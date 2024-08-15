744233b770cd913949eeb4309655e874ae440de0
Dayakar H <dayakarheeraakri@gmail.com>
2024-08-15 13:24:10 +0530

    create layout components

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex h-16 items-center border-t">
+      <div className="container flex items-center">
+        <a>Planet Search</a>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="flex h-16 items-center border-b shadow-sm">
+      <div className="container flex items-center font-display tracking-wide">
+        <a>Planet Search</a>
+      </div>
+    </header>
+  );
+}
