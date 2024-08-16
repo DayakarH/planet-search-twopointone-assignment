4401623c0cbca71517ba2f293dbf5e22ed79acaa
Dayakar H <dayakarheeraakri@gmail.com>
2024-08-16 11:39:57 +0530

    use custom hook to update search term

--- src/components/ui/search-bar.tsx
+++ src/components/ui/search-bar.tsx
@@ -1,26 +1,19 @@
-import { useAppDispatch, useAppSelector } from "@/hooks/redux-hooks";
-import { setSearchText } from "@/store/search-and-filters-slice";
+import { useAppSelector } from "@/hooks/redux-hooks";
+import useUpdateSearchAndFilters from "@/hooks/use-update-search-and-filters";
 import { SearchIcon } from "lucide-react";
 import { useState } from "react";
 import { Button } from "./button";
 import { Input } from "./input";
 
 export default function SearchBar() {
-  const dispatch = useAppDispatch();
+  const { updateSearchTerm } = useUpdateSearchAndFilters();
   const searchText = useAppSelector(
     (state) => state.searchAndFiltersSlice.searchText,
   );
   const [value, setValue] = useState(searchText);
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    dispatch(setSearchText(value));
-
-    const searchParams = new URLSearchParams(window.location.search);
-    searchParams.delete("q");
-    if (value) searchParams.append("q", value);
-
-    const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
-    window.history.pushState({}, "", newUrl);
+    updateSearchTerm(value);
   };
   return (
     <form onSubmit={handleSubmit} className="flex max-w-lg items-center gap-2">
