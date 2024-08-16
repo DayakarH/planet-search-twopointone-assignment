073ab3528753f62fee310fc277de21780888e3ea
Dayakar H <dayakarheeraakri@gmail.com>
2024-08-16 11:38:42 +0530

    extract params update logic into it's own hook

---
+++ src/hooks/use-update-search-and-filters.ts
@@ -0,0 +1,60 @@
+import type { FilterCategories } from "@/lib/types";
+import {
+  setAnyFiltersSelected,
+  setFilters,
+  setSearchText,
+} from "@/store/search-and-filters-slice";
+import { useCallback } from "react";
+import { useAppDispatch, useAppSelector } from "./redux-hooks";
+
+export default function useUpdateSearchAndFilters() {
+  const dispatch = useAppDispatch();
+  const filters = useAppSelector(
+    (state) => state.searchAndFiltersSlice.filters,
+  );
+  const updateSearchTerm = useCallback(
+    (value: string) => {
+      dispatch(setSearchText(value));
+      const searchParams = new URLSearchParams(window.location.search);
+      searchParams.delete("q");
+      if (value) {
+        searchParams.append("q", value);
+        dispatch(setAnyFiltersSelected(true));
+      }
+
+      const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+      window.history.pushState({}, "", newUrl);
+    },
+    [dispatch],
+  );
+  const updateFilters = useCallback(
+    (type: FilterCategories, id: string, checked: boolean) => {
+      const newFilters = {
+        ...filters,
+        [type]: checked
+          ? [...filters[type], id]
+          : filters[type].filter((filterID) => filterID !== id),
+      };
+      console.log(newFilters);
+      dispatch(setFilters(newFilters));
+      dispatch(
+        setAnyFiltersSelected(
+          Object.values(newFilters).some((filter) => filter.length > 0),
+        ),
+      );
+
+      const searchParams = new URLSearchParams(window.location.search);
+      searchParams.delete(type);
+      newFilters[type].forEach((id) => searchParams.append(type, id));
+
+      const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+      window.history.pushState({}, "", newUrl);
+    },
+    [filters, dispatch],
+  );
+
+  return {
+    updateFilters,
+    updateSearchTerm,
+  };
+}
