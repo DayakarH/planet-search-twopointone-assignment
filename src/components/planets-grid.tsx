ffe25125e9335b0bfecb481b1303c106426fd6b7
Dayakar H <dayakarheeraakri@gmail.com>
2024-08-16 11:40:44 +0530

    add empty state UI

--- src/components/planets-grid.tsx
+++ src/components/planets-grid.tsx
@@ -1,3 +1,4 @@
+import { useAppSelector } from "@/hooks/redux-hooks";
 import type { Planet } from "@/lib/types";
 import {
   Card,
@@ -12,13 +13,28 @@ export default function PlanetsGrid({
 }: {
   planets: Array<Planet> | undefined;
 }) {
+  const { searchText, anyFilterSelected } = useAppSelector(
+    (state) => state.searchAndFiltersSlice,
+  );
+
+  if (planets?.length === 0 && (searchText.length || anyFilterSelected)) {
+    return (
+      <div className="mt-16 grid max-h-[50dvh] w-full place-items-center">
+        <p className="text-destructive-foreground">
+          No planets match your filters or search results
+        </p>
+      </div>
+    );
+  }
   return (
     <ul className="grid grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-4 md:grid-cols-[repeat(auto-fill,minmax(220px,1fr))]">
       {planets?.map(({ name, id, description }) => (
         <li key={id}>
           <Card className="h-full">
             <CardHeader>
-              <CardTitle className="font-display tracking-wide">{name}</CardTitle>
+              <CardTitle className="font-display tracking-wide">
+                {name}
+              </CardTitle>
             </CardHeader>
             <CardContent>
               <img
